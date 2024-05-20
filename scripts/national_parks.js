2911ec2398615fffe70ba7a4d735d284482140f2
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-05-20 16:29:11 -0500

    First drop down working and done

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -5,28 +5,30 @@ window.onload = () => {
     initParkselect();
 
     //Grabbing the dropdowns individually from the html page
-   let selectByLocation = document.querySelector("#selectPark");
+    let selectByLocation = document.querySelector("#selectPark");
 
-   let selectByType = document.querySelector("#selectType");
+    let selectByType = document.querySelector("#selectType");
 
-    selectByLocation.addEventListener("change",getInfoOffList)
+    //once a option on the drop down is chosen the information will get displayed
+    selectByLocation.addEventListener("change", getInfoOffList)
 
 }
 function getInfoOffList(event) {
 
-    //get the selected category from the dropdown which is also the event.target
+    //get the selected information from the dropdown which is also the event.target
     let selectedState = event.target.value;
 
+    //Here we are looping through the national park array an filtering through to get the information we need in order to match
     let matchingActivities = nationalParksArray.filter((park) => {
 
         //returning the every single option that it goes through and choosing the 
-        //category and if it mathes the value in the selectedCategory then it returns it.
+        //state and if it mathes the value in the selectedState then it returns it.
         return park.State === selectedState;
 
 
 
     })
-    //getting a hold of the table body so we can add rows to it for all the activities
+    //getting a hold of the table body so we can add rows to it for all the matching state info
     let tableBody = document.querySelector("#infoTableBody");
     //set the innterHTML to "" which clear it out
     tableBody.innerHTML = "";
@@ -34,30 +36,44 @@ function getInfoOffList(event) {
     matchingActivities.forEach((park) => {
 
         //running the function with the table that we grabbed from the HTML 
-        //now the "acitivity" is going throught the list individually
+        //now the "park" is going throught the list individually
         buildTableRow(tableBody, park);
 
 
     })
 
+    //function we are running to add the information from the nationalParkArray to the table
+    //tablebody is the html table we grabbed earlier
+    //data is a parameter that we run what we choose in it
     function buildTableRow(tableBody, data) {
 
         //create the row to hold the data
         let newRow = tableBody.insertRow(-1);
 
-        //loop over all the properties in the object and create a cell for them 
-        //data is the whole thing (the object) and property is grabbing them singularly 
-        for (let property in data) {
+        let cell1 = newRow.insertCell(0);
+        //this is us displaying the text on the page that is collected from the data in JS
+        cell1.innerHTML = data.LocationID
 
-            //grabbing the strings individually and fitting them in a cell
-            let newTD = newRow.insertCell();
-            newTD.innerText = data[property];
+        let cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.LocationName
+
+        let cell3 = newRow.insertCell(2);
+        cell3.innerHTML = `${data.Address}, ${data.City}, ${data.State}, ${data.ZipCode}`
+
+        let cell4 = newRow.insertCell(3)
+        cell4.innerHTML = `Phone: ${data.Phone}, Fax: ${data.Fax}`
+
+        let cell5 = newRow.insertCell(4)
+        if (typeof data.Visit === "undefined"){
+            cell5.innerHTML = "N/A"
+        }else{
+            cell5.innerHTML = data.Visit;
         }
-
- }
+    }
 
 }
-function initParkselect(){
+//created a function for the first drop down, to input the states we choose from
+function initParkselect() {
 
     //we are grabbing the dropdown from the HTML page to work with it
     let selectByLocation = document.querySelector("#selectPark");
@@ -73,8 +89,8 @@ function initParkselect(){
     //add the option we created to the dropdown
     selectByLocation.appendChild(defaultOption);
 
-     //write a loop to work with each individual category and build an option for it
-     locationsArray.forEach((state) => {
+    //write a loop to work with each individual category and build an option for it
+    locationsArray.forEach((state) => {
 
         //create the new option for the category we are on in the loop
         let newOption = document.createElement("option");
@@ -112,17 +128,17 @@ function matchByStateName(nationalParksArray, state) {
     return matching;
 }
 
-   /*
-        You can remove the following console.log() lines.
-        They are here to verify that we have access to the data
-        The data script files are located in the scripts/data directory
-    */
+/*
+     You can remove the following console.log() lines.
+     They are here to verify that we have access to the data
+     The data script files are located in the scripts/data directory
+ */
 
-    //log the locationsArray to the console (scripts/data/locationData.js)
-    // console.log(locationsArray)
+//log the locationsArray to the console (scripts/data/locationData.js)
+// console.log(locationsArray)
 
-    //log the parkTypesArray to the console (scripts/data/parkTypeData.js)
-    // console.log(parkTypesArray)
+//log the parkTypesArray to the console (scripts/data/parkTypeData.js)
+// console.log(parkTypesArray)
 
-    //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
-    // console.log(nationalParksArray)
+//log the nationalParksArray to the console (scripts/data/nationalParkData.js)
+// console.log(nationalParksArray)
