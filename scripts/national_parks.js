39b51685ed54ef68001c5e67c91c02a48baf0306
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-05-21 16:01:07 -0500

    added the mountains page, drop down and card

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -6,15 +6,24 @@ window.onload = () => {
 
     initTypeSelect();
 
+    hideOrShowElement();
+
     //Grabbing the dropdowns individually from the html page
     let selectByLocation = document.querySelector("#selectPark");
 
     let selectByType = document.querySelector("#selectType");
 
+    //grabbing our buttons off the html page
+    let locationButton = document.querySelector("#location");
+
+    let parkTypeButton = document.querySelector("#type");
+
     //once a option on the drop down is chosen the information will get displayed
     selectByLocation.addEventListener("change", getInfoOffList);
 
     selectByType.addEventListener("change", gettypeOffList);
+
+
 
 }
 function getInfoOffList(event) {
@@ -173,21 +182,25 @@ function gettypeOffList(event) {
     //get the selected information from the dropdown which is also the event.target
     let selectedType = event.target.value;
 
+    //here we are running a function to match the park types and the national park type
     let matchingTypes = nationalParksArray.filter((type) => {
 
+        //running a loop through the parktypes to get the individual strings
         for (let i = 0; i < parkTypesArray.length; i++) {
+            //if the nationalparks locationName has a matching index with the selectedType(The dropdown choices) then return true
+            //the negative one is basically saying "not present" but the "!" is saying "not"
             if (type.LocationName.indexOf(selectedType) !== -1) {
 
                 return true
             }
             return false
-        }
+        };
 
 
 
-    })
+    });
 
-    //getting a hold of the table body so we can add rows to it for all the matching state info
+    //getting a hold of the table body so we can add rows to it for all the matching parktype info
     let tableBody = document.querySelector("#infoTableBody");
     //set the innterHTML to "" which clear it out
     tableBody.innerHTML = "";
@@ -195,37 +208,71 @@ function gettypeOffList(event) {
     matchingTypes.forEach((type) => {
 
         //running the function with the table that we grabbed from the HTML 
-        //now the "park" is going throught the list individually
+        //now the "type" is going throught the list individually
         buildTableRow(tableBody, type);
 
     })
 }
 //function we are running to add the information from the nationalParkArray to the table
-    //tablebody is the html table we grabbed earlier
-    //data is a parameter that we run what we choose in it
-    function buildTableRow(tableBody, chosen) {
+//tablebody is the html table we grabbed earlier
+//chosen is a parameter that we run what we choose in it
+function buildTableRow(tableBody, chosen) {
 
-        //create the row to hold the data
-        let newRow = tableBody.insertRow(-1);
+    //create the row to hold the data
+    let newRow = tableBody.insertRow(-1);
 
-        let cell1 = newRow.insertCell(0);
-        //this is us displaying the text on the page that is collected from the data in JS
-        cell1.innerHTML = chosen.LocationID
+    let cell1 = newRow.insertCell(0);
+    //this is us displaying the text on the page that is collected from the data in JS
+    cell1.innerHTML = chosen.LocationID
 
-        let cell2 = newRow.insertCell(1);
-        cell2.innerHTML = chosen.LocationName
+    let cell2 = newRow.insertCell(1);
+    cell2.innerHTML = chosen.LocationName
 
-        let cell3 = newRow.insertCell(2);
-        cell3.innerHTML = `${chosen.Address}, ${chosen.City}, ${chosen.State}, ${chosen.ZipCode}`
+    let cell3 = newRow.insertCell(2);
+    cell3.innerHTML = `${chosen.Address}, ${chosen.City}, ${chosen.State}, ${chosen.ZipCode}`
 
-        let cell4 = newRow.insertCell(3)
-        cell4.innerHTML = `Phone: ${chosen.Phone}, Fax: ${chosen.Fax}`
+    let cell4 = newRow.insertCell(3)
+    cell4.innerHTML = `Phone: ${chosen.Phone}, Fax: ${chosen.Fax}`
 
-        //creating an if statement for the url section as some dont have any and I dont wanna show "undefined"
-        let cell5 = newRow.insertCell(4)
-        if (!chosen.Visit) {
-            cell5.innerHTML = "N/A"
-        } else {
-            cell5.innerHTML = chosen.Visit;
-        }
+    //creating an if statement for the url section as some dont have any and I dont wanna show "undefined"
+    let cell5 = newRow.insertCell(4)
+    if (!chosen.Visit) {
+        cell5.innerHTML = "N/A"
+    } else {
+        cell5.innerHTML = chosen.Visit;
+
+
+
     }
+}
+
+
+//This function will hide or show an HTML element on the page
+//Just pass it the id of the element you want to show/hide
+function hideOrShowElement() {
+    let el = document.querySelector("#selectPark");
+    let la = document.querySelector("#selectType");
+    let locationButton = document.querySelector("#location");
+    let parkTypeButton = document.querySelector("#type");
+
+
+    if (locationButton.checked) {
+        el.style.display = "block";
+    } else {
+        el.style.display = "none";
+    }
+
+    // if(parkTypeButton.value === "type"){
+    //     la.style.display = "block";
+    // }else {
+    //     la.style.display = "none";
+    // }
+
+    // function yesnoCheck() {
+    //     if (document.getElementById('yesCheck').checked) {
+    //         document.getElementById('ifYes').style.display = 'block';
+    //     } else {
+    //         document.getElementById('ifYes').style.display = 'none';
+    //     }
+    // }
+}
